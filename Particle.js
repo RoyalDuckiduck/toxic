cd4b1b905afd2d767ab709f618cd381f818da1d3
RoyalDuck <royalduckyduck@gmail.com>
2023-10-28 16:39:54 +0200

    no message

--- Particle.js
+++ Particle.js
@@ -1,15 +1,13 @@
-class Particle {
-  constructor(x, y, radius = 10, gravity = true, draw = false) {
-    this.x = x;
-    this.y = y;
+class Particle extends toxi.physics2d.VerletParticle2D{
+  constructor(x, y, radius = 10, draw = false) {
+    super(x, y);
     this.r = radius;
-    this.g = gravity;
     this.d = draw;
   }
   draw(){
     //doesn't draw to avoid drawing using a loop.
     if (this.d){
-      circle(this.x, this.y, r);
+      circle(this.x, this.y, this.r*2);
     }
   }
 }
--- sketch.js
+++ sketch.js
@@ -4,6 +4,9 @@ const { Vec2D } = toxi.geom;
 
 let world = new VerletPhysics2D();
 
+let particles = [];
+particles.push(new Particle(100,100,5,true));
+
 function setup() {
   createCanvas(400,400);
   bg = color(0,255,255);
@@ -11,4 +14,10 @@ function setup() {
 
 function draw() {
   background(bg);
+  world.update();
+  fill(100);
+  stroke(0,0);
+  particles.forEach(function(particle){
+    particle.draw();
+  });
 }
